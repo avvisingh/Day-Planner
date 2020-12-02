648cd9aefebce0f504d10aa6fbb52c538ecdc3d5
Avvi Singh <singh.avvi98@gmail.com>
2020-12-02 11:43:52 +1100

    Init frontend layout

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ let currentTime = moment().format('MMMM Do YYYY, h:mm a');
 console.log(currentTime);
 
 $currentDayTime.text(currentTime);
+
+let $momentTest = moment("12-25-1995", "MM-DD-YYYY");
+console.log($momentTest);  
