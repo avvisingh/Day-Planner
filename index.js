b65686cf8a4f33b46e6ce5a4b9111cac0956a8df
Avvi Singh <singh.avvi98@gmail.com>
2020-12-03 10:07:44 +1100

    Background Colours of the event divs now update according to time

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+//Selecting the <p> in the header with the currentDay id
 const $currentDayTime = $('#currentDay');
 
-let currentTime = moment().format('MMMM Do YYYY, h:mm a');
-console.log(currentTime);
+//Setting the Current Date and Time display in the <header>
+let $currentDateTime = moment().format('MMMM Do YYYY, h:mm a');
+$currentDayTime.text($currentDateTime);
+console.dir($currentDateTime);
 
-$currentDayTime.text(currentTime);
+let $currentTime = moment().format('LT');
+console.log($currentTime);
 
-let $momentTest = moment("12-25-1995", "MM-DD-YYYY");
-console.log($momentTest);  
+//Assigning background colours to the event sections based on the time
+var $m = moment().format('HH');
+console.log(`m = ${$m}`);
+
+// const A = 10;
+// const selection = `div#time-display-${A}`;
+// console.log(selection);
+// $(selection.toString()).html('<p>Avvi</p>');
+
+
+//Assigning background colours to the event sections based on the time
+var i;
+for (i = 9; i <= 16; i++) {
+    let selection = `div#event-display-${i}`;
+    console.log(selection);
+
+    if ($m == i) {
+        $(selection.toString()).css('background-color', 'rgba(63, 232, 148, 0.4)');
+    } else if ($m > i) {
+        $(selection.toString()).css('background-color', 'rgba(245, 86, 112, 0.4)');
+    } else {
+        $(selection.toString()).css('background-color', 'rgba(63, 114, 232, 0.4)');
+    }
+};
+
+
